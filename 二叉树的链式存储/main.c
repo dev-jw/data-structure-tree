d369e980996905b8506bbb0f04c46a99ef561f34
Lemon <573986558@qq.com>
2020-04-29 19:50:47 +0800

    二叉树

--- 二叉树的链式存储/main.c
+++ 二叉树的链式存储/main.c
@@ -7,21 +7,314 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef int Status;
+#define OK 1;
+#define ERROR 0;
+#define TRUE 1;
+#define FALSE 0;
 
 #define MAX_TREE_SIZE 100
-
-typedef int TElemType;
-
+typedef char TElemType;
 TElemType Nil = 0;
 
-typedef struct TreeNode {
+typedef struct BTreeNode {
     TElemType data;
-    struct TreeNode *leftTree;
-    struct TreeNode *rightTree;
-}BitTreeNode, *BitTree;
+    struct BTreeNode *leftTree;
+    struct BTreeNode *rightTree;
+}BTreeNode, *BitTree;
+
+// 构建一个二叉树的字符串
+typedef TElemType String[MAX_TREE_SIZE];
+Status strAssign(String S, char *chars) {
+    if (strlen(chars) > MAX_TREE_SIZE) {
+        return ERROR;
+    }else {
+        int i = 0;
+        char *ch = chars;
+        while (*ch != '\0') {
+            S[i] = *ch;
+            ch++;
+            i++;
+        }
+        return TRUE;
+    }
+    return OK;
+}
+
+// 打印
+void visit(TElemType e) {
+    printf("%c ", e);
+}
+
+// 构造二叉树
+Status initTree(BitTree *T) {
+    *T = NULL;
+    return OK;
+}
+
+// 清空二叉树
+Status clearTree(BitTree *T) {
+    if (*T) {
+        if ((*T)->leftTree) {
+            clearTree(&(*T)->leftTree);
+        }
+        if ((*T)->rightTree) {
+            clearTree(&(*T)->rightTree);
+        }
+        free(*T);
+        *T = NULL;
+    }
+    return OK;
+}
+
+// 销毁二叉树
+#define DestroyTree clearTree
+
+// 创建二叉树 - 按前序遍历的方式插入
+String str;
+int idx = 0;
+Status createTree(BitTree *T) {
+    TElemType ch = str[idx++];
+
+    if (ch != '#') {
+        *T = (BitTree)malloc(sizeof(BTreeNode));
+        (*T)->data = ch;
+        
+        createTree(&(*T)->leftTree);
+        createTree(&(*T)->rightTree);
+    }else {
+        *T = NULL;
+    }
+    return OK;
+}
+
+// 二叉树是否为空
+Status isEmptyTree(BitTree T) {
+    if (T) {
+        return FALSE;
+    }else return TRUE;
+}
+
+// 二叉树的深度
+int deepOfTree(BitTree T) {
+    int left, right;
+    
+    if (!T) {
+        return 0;
+    }
+    
+    if (T->leftTree) {
+        left = deepOfTree(T->leftTree);
+    }else {
+        left = 0;
+    }
+    
+    if (T->rightTree) {
+        right = deepOfTree(T->rightTree);
+    }else {
+        right = 0;
+    }
+    
+    return left > right ? left + 1 : right + 1;
+}
+
+// 二叉树的前序遍历
+void preOrderTraverse(BitTree T) {
+    if (T) {
+        visit(T->data);
+        preOrderTraverse(T->leftTree);
+        preOrderTraverse(T->rightTree);
+    }
+}
+
+// 非递归实现
+// 利用栈的思想
+void preOrder(BitTree T) {
+    BTreeNode *stack[MAX_TREE_SIZE];
+    int top = -1;
+    stack[++top] = T;
+    
+    BTreeNode *temp;
+    while (top != -1) {
+        while ((temp = stack[top]) != NULL) {
+            visit(temp->data);
+            stack[++top] = temp->leftTree;
+        }
+        top--;
+        if (top != -1) {
+            temp = stack[top--];
+            stack[++top] = temp->rightTree;
+        }
+    }
+}
+
+void preOrderEx(BitTree T) {
+    BTreeNode *stack[MAX_TREE_SIZE];
+    int top = -1;
+    BTreeNode *temp = T;
+    while (temp || top != -1) {
+        if (temp) {
+            stack[++top] = temp;
+            visit(temp->data);
+            temp = temp->leftTree;
+        }else {
+            temp = stack[top--];
+            temp = temp->rightTree;
+        }
+    }
+}
+
+// 二叉树的中序遍历
+void inOrderTraverse(BitTree T) {
+    if (T) {
+        inOrderTraverse(T->leftTree);
+        visit(T->data);
+        inOrderTraverse(T->rightTree);
+    }
+}
+
+void inOrder(BitTree T) {
+    BTreeNode *stack[MAX_TREE_SIZE];
+    int top = -1;
+    stack[++top] = T;
+    
+    BTreeNode *temp;
+    while (top != -1) {
+        while ((temp = stack[top]) != NULL) {
+            stack[++top] = temp->leftTree;
+        }
+        top--;
+        if (top != -1) {
+            temp = stack[top--];
+            visit(temp->data);
+            stack[++top] = temp->rightTree;
+        }
+    }
+}
+
+void inOrderEx(BitTree T) {
+    BTreeNode *stack[MAX_TREE_SIZE];
+    int top = -1;
+    BTreeNode *temp = T;
+    while (temp || top != -1) {
+        if (temp) {
+            stack[++top] = temp;
+            temp = temp->leftTree;
+        }else {
+            temp = stack[top--];
+            visit(temp->data);
+            temp = temp->rightTree;
+        }
+    }
+}
+
+// 二叉树的后序遍历
+void postOrderTraverse(BitTree T) {
+    if (T) {
+        postOrderTraverse(T->leftTree);
+        postOrderTraverse(T->rightTree);
+        visit(T->data);
+    }
+}
+
+// 非递归实现
+/**
+ 1. 先从左遍历到头
+ 2. 判断当前的结点是否有右子树，或者说有右子树并且没有被访问，则进栈，再从左遍历到头
+ 3. 到头后如果没有孩子，或者有右子树但是被访问过，则输出结点信息，并将游标指向它
+ */
+void postOrder(BitTree T) {
+    BTreeNode *stack[MAX_TREE_SIZE];
+    int top = -1;
+    BTreeNode *temp = T;
+    BTreeNode *cur  = NULL; // 游标指向上一个被访问的结点
+    
+    while (temp || top != -1) {
+        if (temp) {
+            stack[++top] = temp;
+            temp = temp->leftTree;
+        }else {
+            temp = stack[top];
+            if (temp->rightTree && temp->rightTree != cur) {
+                temp = temp->rightTree;
+                stack[++top] = temp;
+                temp = temp->leftTree;
+            }else {
+                temp = stack[top--];
+                visit(temp->data);
+                cur = temp;
+                temp = NULL;
+            }
+        }
+    }
+}
+
+// 层次遍历
+void levalTree(BitTree T, int leval) {
+    if (T == NULL) {
+        return;
+    }else if (leval == 1) {
+        visit(T->data);
+    }else {
+        levalTree(T->leftTree, leval -1);
+        levalTree(T->rightTree, leval -1);
+    }
+}
+
+void levalOrder(BitTree T) {
+    int deep = deepOfTree(T);
+    int i = 1;
+    while (i != deep) {
+        levalTree(T, i++);
+    }
+}
+
+
+
 
 int main(int argc, const char * argv[]) {
     // insert code here...
     printf("Hello, World!\n");
+    
+    BitTree T;
+    initTree(&T);
+        
+    strAssign(str, "ABDH#K###E##CFI###G#J##");
+    createTree(&T);
+
+    printf("=====先序遍历=====\n");
+    preOrderTraverse(T);
+    printf("\n");
+
+    preOrder(T);
+    printf("\n");
+    
+    preOrderEx(T);
+    printf("\n");
+    
+    printf("=====中序遍历=====\n");
+    inOrderTraverse(T);
+    printf("\n");
+    
+    inOrder(T);
+    printf("\n");
+    
+    inOrderEx(T);
+    printf("\n");
+    
+    printf("=====后序遍历=====\n");
+    postOrderTraverse(T);
+    printf("\n");
+    
+    postOrder(T);
+    printf("\n");
+    
+    printf("=====层次遍历=====\n");
+    levalOrder(T);
+    printf("\n");
+
     return 0;
 }
--- 二叉树的顺序存储/main.c
+++ 二叉树的顺序存储/main.c
@@ -7,15 +7,227 @@
 //
 
 #include <stdio.h>
+#include <string.h>
+#include <math.h>
 
+typedef int Status;
+#define OK 1;
+#define ERROR 0;
+#define TRUE 1;
+#define FALSE 0;
 
 #define MAX_TREE_SIZE 100
-typedef int TElemType;
-TElemType Nil = 0;
+typedef char TElemType;
+TElemType Nil = '\0';
 typedef TElemType SeqBiTree[MAX_TREE_SIZE];
+
+void visit(TElemType e) {
+    printf("%c ", e);
+}
+
+// 构造二叉树
+Status initTree(SeqBiTree T) {
+    for (int i = 0; i < MAX_TREE_SIZE; i++) {
+        T[i] = Nil;
+    }
+    return OK;
+}
+
+// 清空二叉树
+#define clearTree initTree
+
+// 创建二叉树 - 按前序遍历的方式插入
+Status createTree(SeqBiTree T, char *S) {
+    /*
+   1  0                  A
+   2  1 2            B       C
+   3  3 4 5 6     D    E   F    G
+     */
+    char *p = S;
+    int i = 0;
+    while (p != '\0' && i < strlen(S)) {
+        T[i] = *p;
+        if (i != 0 && T[(i + 1) / 2 - 1] == Nil && T[i] != Nil) {
+            printf("出现无双亲结点的非根结点");
+            return ERROR;
+        }
+        i++;
+        p++;
+    }
+    
+    while (i < MAX_TREE_SIZE) {
+        T[i] = Nil;
+        i++;
+    }
+    return OK;
+}
+
+// 二叉树是否为空
+int isEmptyTree(SeqBiTree T) {
+    if (T[0] == Nil) {
+        return TRUE;
+    }else return FALSE;
+}
+
+// 二叉树的深度
+/* 深度为h的二叉树最多有2^h-1个结点（h>=1），最少有h个结点 */
+int deepOfTree(SeqBiTree T) {
+    /* 深度 */
+    int j = -1;
+    /* 记录最后一个结点位置 */
+    int i;
+    
+    for (i = MAX_TREE_SIZE; i >=0; i--) {
+        if (T[i] != Nil) {
+            break;
+        }
+    }
+    
+    do {
+        j++;
+    } while (powl(2, j) <= i);
+    return j;
+}
+
+// 处于{层，本层序号}的结点值
+TElemType valueOfPosition(SeqBiTree T, int leval, int order) {
+    return T[(int)pow(2, leval) + order - 2];
+}
+
+/* 获取结点值e的双亲结点的值 */
+TElemType parent(SeqBiTree T, TElemType e) {
+    if (T[0] == Nil) {
+        return Nil;
+    }
+    for (int i = 0; i < MAX_TREE_SIZE; i++) {
+        if (T[i] == e) {
+            return T[ (i + 1) / 2 -1];
+        }
+    }
+    return Nil;
+}
+
+/* 获取某个结点的左孩子 */
+TElemType leftChild(SeqBiTree T, TElemType e){
+        if (T[0] == Nil) {
+        return Nil;
+    }
+    for (int i = 0 ; i < MAX_TREE_SIZE-1; i++) {
+        if (T[i] == e) {
+            return T[ i * 2 + 1 ];
+        }
+    }
+    return Nil;
+}
+
+/* 获取某个结点的右孩子 */
+TElemType rightChild(SeqBiTree T, TElemType e){
+        if (T[0] == Nil) {
+        return Nil;
+    }
+    for (int i = 0 ; i < MAX_TREE_SIZE-1; i++) {
+        if (T[i] == e) {
+            return T[ i * 2 + 2 ];
+        }
+    }
+    return Nil;
+}
+
+// 二叉树的前序遍历
+void PreTraverse(SeqBiTree T,int e){
+    
+    //打印结点数据
+    visit(T[e]);
+    
+    //先序遍历左子树
+    if (T[2 * e + 1] != Nil) {
+        PreTraverse(T, 2*e+1);
+    }
+    //最后先序遍历右子树
+    if (T[2 * e + 2] != Nil) {
+        PreTraverse(T, 2*e+2);
+    }
+}
+
+Status PreOrderTraverse(SeqBiTree T){
+    
+    //树不为空
+    if (!isEmptyTree(T)) {
+        PreTraverse(T, 0);
+    }
+    printf("\n");
+    return  OK;
+}
+
+// 二叉树的中序遍历
+void InTraverse(SeqBiTree T, int e){
+    
+    /* 左子树不空 */
+    if (T[2*e+1] != Nil)
+        InTraverse(T, 2*e+1);
+    
+    visit(T[e]);
+    
+    /* 右子树不空 */
+    if (T[2*e+2] != Nil)
+        InTraverse(T, 2*e+2);
+}
+
+Status InOrderTraverse(SeqBiTree T){
+    
+    /* 树不空 */
+    if (!isEmptyTree(T)) {
+        InTraverse(T, 0);
+    }
+    printf("\n");
+    return OK;
+}
+
+// 二叉树的后序遍历
+void PostTraverse(SeqBiTree T,int e)
+{   /* 左子树不空 */
+    if(T[2*e+1]!=Nil)
+        PostTraverse(T,2*e+1);
+    /* 右子树不空 */
+    if(T[2*e+2]!=Nil)
+        PostTraverse(T,2*e+2);
+    
+    visit(T[e]);
+}
+Status PostOrderTraverse(SeqBiTree T)
+{
+    if(!isEmptyTree(T)) /* 树不空 */
+        PostTraverse(T,0);
+    printf("\n");
+    return OK;
+}
+
+// 层次遍历
+void levalOrder(SeqBiTree T) {
+    /* 找到最后一个非空结点的序号 */
+    int i = MAX_TREE_SIZE - 1;
+    while (T[i] == Nil) {
+        i--;
+    }
+    
+    /* 从根结点起,按层序遍历二叉树 */
+    for (int j = 0; j <= i; j++) {
+        /* 只遍历非空结点 */
+        if (T[j] != Nil) {
+            visit(T[j]);
+        }
+    }
+}
 
 int main(int argc, const char * argv[]) {
     // insert code here...
     printf("Hello, World!\n");
+    SeqBiTree T;
+    
+    initTree(T);
+    
+    createTree(T, "ABCDEFG");
+    
     return 0;
 }
+    
